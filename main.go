d4d559e5be39c94b3925167e72f7da73ae24f4ee
kordian <k.dziwisz.635@studms.ug.edu.pl>
2024-04-11 10:45:50 +0200

    launch config and some work with the main

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func readUserInput() string {
+	return os.Args[1]
+}
+
+func formatInput(input *string) *string {
+	formatedInput := ""
+	for _, char := range *input {
+		fmt.Println(string(float32(int(char))))
+		formatedInput = formatedInput + string(int(char))
+	}
+	return &formatedInput
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	input := readUserInput()
+	fmt.Println(formatInput(&input))
 }
