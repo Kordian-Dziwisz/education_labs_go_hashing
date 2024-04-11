adeb3ace818e474aa1888195e1934be81db36242
kordian <k.dziwisz.635@studms.ug.edu.pl>
2024-04-11 12:02:57 +0200

    added launch, finished 1st part of the task

--- main.go
+++ main.go
@@ -2,23 +2,65 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func readUserInput() string {
-	return os.Args[1]
+	return strings.ToLower(os.Args[1])
 }
 
 func formatInput(input *string) *string {
 	formatedInput := ""
 	for _, char := range *input {
-		fmt.Println(string(float32(int(char))))
-		formatedInput = formatedInput + string(int(char))
+		formatedInput = formatedInput + strconv.FormatInt(int64(char), 10)
 	}
 	return &formatedInput
+}
+
+func silnia(n int64) *big.Int {
+	value := new(big.Int).SetInt64(1)
+	for i := n; i > 0; i-- {
+		value = value.Mul(value, new(big.Int).SetInt64(i))
+	}
+	return value
+}
+
+func findRecursive(s string, searchedS string) bool {
+	if len(searchedS) == 0 {
+		return false
+	}
+	char := rune(searchedS[0])
+	idx, err := findAsciiInStr(s, char)
+	if err {
+		return true
+	} else {
+		fmt.Println("index for ", char, ": ", idx)
+		return findRecursive(s[idx+3:], searchedS[1:])
+	}
+}
+
+func findAsciiInStr(s string, char rune) (int, bool) {
+	chars := strconv.FormatInt(int64(char), 10)
+	i := strings.Index(s, chars)
+	err := false
+	if i == -1 {
+		err = true
+	}
+	return i, err
 }
 
 func main() {
 	input := readUserInput()
 	fmt.Println(formatInput(&input))
+	for i := int64(0); i < 10000; i++ {
+		fmt.Println("for ", i, "!:")
+		s := silnia(i).String()
+		if !findRecursive(s, input) {
+			fmt.Println("Found it!")
+			break
+		}
+	}
 }
